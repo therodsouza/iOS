a97f41434e5a52b50995d765f714951801cdf4f5
ios5778 <ios5778@mac-mini-de-caelum-5.local>
2015-10-24 10:02:34 -0200

    Initial Commit

---
+++ ContatosIP67/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ContatosIP67
+//
+//  Created by ios5778 on 24/10/15.
+//  Copyright © 2015 Venturus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ContatosIP67/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ContatosIP67
+//
+//  Created by ios5778 on 24/10/15.
+//  Copyright © 2015 Venturus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
