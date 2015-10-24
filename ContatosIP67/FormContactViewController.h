12c2665f08b81c055642b956e553864c5f27a471
ios5778 <ios5778@Mac-mini-de-caelum-5.local>
2015-10-24 14:29:20 -0200

    Exercicio 1

---
+++ ContatosIP67/FormContactViewController.h
@@ -0,0 +1,25 @@
+//
+//  ViewController.h
+//  ContatosIP67
+//
+//  Created by ios5778 on 24/10/15.
+//  Copyright © 2015 Venturus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Contact.h"
+
+@interface FormContactViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *textFieldName;
+@property (weak, nonatomic) IBOutlet UITextField *textFieldEmail;
+@property (weak, nonatomic) IBOutlet UITextField *textFieldSite;
+@property (weak, nonatomic) IBOutlet UITextField *textFieldAddress;
+@property (weak, nonatomic) IBOutlet UITextField *textFieldPhone;
+
+
+    -(IBAction) getFormData;
+
+
+@end
+
---
+++ ContatosIP67/FormContactViewController.m
@@ -0,0 +1,43 @@
+//
+//  ViewController.m
+//  ContatosIP67
+//
+//  Created by ios5778 on 24/10/15.
+//  Copyright © 2015 Venturus. All rights reserved.
+//
+
+#import "FormContactViewController.h"
+
+
+
+@implementation FormContactViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction) getFormData {
+    NSString *name = self.textFieldName.text;
+    NSString *email = self.textFieldEmail.text;
+    NSString *site = self.textFieldSite.text;
+    NSString *address = self.textFieldAddress.text;
+    NSString *phone = self.textFieldPhone.text;
+    
+    Contact *contact = [Contact new];
+    
+    contact.name = name;
+    contact.email = email;
+    contact.site = site;
+    contact.address =address;
+    contact.phone = phone;
+    
+    NSLog(@"Contact %@", contact);
+}
+
+@end
