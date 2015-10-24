98abdf27cc16a1172bff82767bf767241274398d
ios5778 <ios5778@Mac-mini-de-caelum-5.local>
2015-10-24 14:29:37 -0200

    Exercicio 1

---
+++ ContatosIP67/Contact.h
@@ -0,0 +1,18 @@
+//
+//  Contact.h
+//  ContatosIP67
+//
+//  Created by ios5778 on 24/10/15.
+//  Copyright © 2015 Venturus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Contact : NSObject
+@property (strong) NSString *name;
+@property (strong) NSString *email;
+@property (strong) NSString *site;
+@property (strong) NSString *address;
+@property (strong) NSString *phone;
+
+@end
---
+++ ContatosIP67/Contact.m
@@ -0,0 +1,17 @@
+//
+//  Contact.m
+//  ContatosIP67
+//
+//  Created by ios5778 on 24/10/15.
+//  Copyright © 2015 Venturus. All rights reserved.
+//
+
+#import "Contact.h"
+
+@implementation Contact
+
+- (NSString *) description {
+    return [NSString stringWithFormat: @"%@ <%@>", self.name, self.email ];
+}
+
+@end
